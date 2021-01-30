eb81f6e9d3f723d805a83e84bdbca04996b199e4
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-01-29 21:39:35 -0300

    Add files via upload

---
+++ class3.sql
@@ -0,0 +1,6 @@
+insert into pessoas values
+(DEFAULT, 'bruno', '78.5', '1994-01-02', '1.83', 'M', 'Brasil'),
+(default, 'Jonas', '65.8', '1987-04-09', '1.64', 'M', 'Brasil'),
+(default,'Isadora', '56.5', '2005-03-02', '1.65', 'F', 'Brasil');
+
+select * from pessoas;
